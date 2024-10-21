d131997b2f49e919536a543efab42f1c04328736
koblakai <155496529+koblakai@users.noreply.github.com>
2024-10-21 06:58:47 -0400

    Update src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import PublicLayout from './components/public/PublicLayout';
+
+function App() {
+  return (
+    <Router>
+      <PublicLayout />
+    </Router>
+  );
+}
+
+export default App;
