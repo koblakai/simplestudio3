e0b681f1055c121f06dfc6da6a43ced674dd0541
koblakai <155496529+koblakai@users.noreply.github.com>
2024-10-21 10:02:41 -0400

    Update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,12 @@
 export const config = {
   studioName: "",
   locations: [],
-  pricing: [{"price":79.99,"description":["Access to all classes "],"name":"Monthly Unlimited"},{"description":["Access to all classes","Mat rental"],"name":"Monthly Premium","price":120},{"description":[],"name":"Yearly Unlimited","price":999},{"name":"Yearly Premium","price":1099,"description":["Access to All Classes","Mat Rental","Access to 2 Specialized Workshops"]}],
+  pricing: [{"price":79.99,"description":["Access to all classes "],"name":"Monthly Unlimited"},{"price":120,"name":"Monthly Premium","description":["Access to all classes","Mat rental"]},{"description":[],"name":"Yearly Unlimited","price":999},{"description":["Access to All Classes","Mat Rental","Access to 2 Specialized Workshops"],"name":"Yearly Premium","price":1099}],
   socialMedia: [],
   enableBlog: true,
   enableShop: true,
   landingPageImage: "https://images.unsplash.com/photo-1603988363607-e1e4a66962c6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-  classes: [{"capacity":20,"date":"2024-10-21","locationId":"","instructor":"Tiffany Blackwell","description":"Slow flow vinyasa suitable for all!","name":"Vinyasa Flow","room":"Yoga Studio 1","id":1729383204690,"time":"05:00"}],
+  classes: [{"locationId":"","capacity":20,"id":1729383204690,"time":"05:00","name":"Vinyasa Flow","description":"Slow flow vinyasa suitable for all!","date":"2024-10-21","instructor":"Tiffany Blackwell","room":"Yoga Studio 1"}],
   hasMultipleLocations: false,
   currentLocation: undefined,
 };
