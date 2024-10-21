bf64a4314452b6a7cbc077d0af6bb1075ceec5b0
Simple Studio <noreply@simplestudio.com>
2024-10-21 10:52:35 -0400

    Add/update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 export const config = {
   studioName: "",
   locations: [],
-  pricing: [{"price":79.99,"name":"Monthly Unlimited","description":["Access to all classes "]},{"price":120,"name":"Monthly Premium","description":["Access to all classes","Mat rental"]},{"name":"Yearly Unlimited","price":999,"description":[]},{"description":["Access to All Classes","Mat Rental","Access to 2 Specialized Workshops"],"price":1099,"name":"Yearly Premium"}],
+  pricing: [{"description":["Access to all classes "],"price":79.99,"name":"Monthly Unlimited"},{"description":["Access to all classes","Mat rental"],"name":"Monthly Premium","price":120},{"name":"Yearly Unlimited","description":[],"price":999},{"price":1099,"name":"Yearly Premium","description":["Access to All Classes","Mat Rental","Access to 2 Specialized Workshops"]}],
   socialMedia: [],
   enableBlog: true,
   enableShop: true,
