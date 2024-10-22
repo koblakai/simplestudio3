c7d0faadce97f00cc741d390c10e4552a0e8a1f7
Simple Studio <noreply@simplestudio.com>
2024-10-22 10:39:05 -0400

    Add/update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,10 @@
 export const config = {
   studioName: "",
   locations: [],
-  pricing: [{"price":80,"description":["Access to all class for one month"],"name":"Monthly Unlimited"},{"name":"Monthly Premium","price":120,"description":["Access to all classes for one month","Yoga mat rental"]},{"price":999,"description":["Access to all classes for one year "],"name":"Yearly Unlimited"},{"description":["Access to All Classes","Mat Rental","Access to 2 Specialized Workshops"],"price":1199,"name":"Yearly Premium"},{"name":"Walk-in ","price":30,"description":["Access to one class"],"durationMonths":1,"paymentLink":""},{"name":"Walk-in premium","price":50,"description":["Access to one class","Yoga Mat Rental"],"durationMonths":1,"paymentLink":""}],
+  pricing: [{"price":80,"name":"Monthly Unlimited","description":["Access to all class for one month"]},{"price":120,"name":"Monthly Premium","description":["Access to all classes for one month","Yoga mat rental"]},{"name":"Yearly Unlimited","description":["Access to all classes for one year "],"price":999},{"description":["Access to All Classes","Mat Rental","Access to 2 Specialized Workshops"],"price":1199,"name":"Yearly Premium"},{"price":30,"paymentLink":"","durationMonths":1,"description":["Access to one class"],"name":"Walk-in "},{"paymentLink":"","description":["Access to one class","Yoga Mat Rental"],"name":"Walk-in premium","durationMonths":1,"price":50}],
   socialMedia: [],
   enableBlog: false,
   enableShop: false,
   landingPageImage: "https://images.unsplash.com/photo-1524863479829-916d8e77f114?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-  classes: [{"date":"2024-10-21","room":"Yoga Studio 1","id":1729383204690,"time":"05:00","instructor":"Tiffany Blackwell","capacity":20,"description":"Slow flow vinyasa suitable for all!","name":"Vinyasa Flow","locationId":""}],
+  classes: [{"room":"Yoga Studio 1","time":"05:00","date":"2024-10-21","instructor":"Tiffany Blackwell","id":1729383204690,"name":"Vinyasa Flow","locationId":"","capacity":20,"description":"Slow flow vinyasa suitable for all!"}],
 };
