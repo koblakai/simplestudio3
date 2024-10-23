dc7ed0bd2b15ad30909f53db3402f00b8e244cf2
Simple Studio <noreply@simplestudio.com>
2024-10-23 14:05:04 -0400

    Add/update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,9 @@
 
 export const config = {
-  studioName: "",
+  studioName: "Simple Studio",
   locations: [],
-  pricing: [{"price":80,"name":"Monthly Unlimited","description":["Access to all class for one month"]},{"price":120,"name":"Monthly Premium","description":["Access to all classes for one month","Yoga mat rental"]},{"name":"Yearly Unlimited","description":["Access to all classes for one year "],"price":999},{"description":["Access to All Classes","Mat Rental","Access to 2 Specialized Workshops"],"price":1199,"name":"Yearly Premium"},{"price":30,"paymentLink":"","durationMonths":1,"description":["Access to one class"],"name":"Walk-in "},{"paymentLink":"","description":["Access to one class","Yoga Mat Rental"],"name":"Walk-in premium","durationMonths":1,"price":50}],
   socialMedia: [],
   enableBlog: false,
   enableShop: false,
   landingPageImage: "https://images.unsplash.com/photo-1524863479829-916d8e77f114?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-  classes: [{"room":"Yoga Studio 1","time":"05:00","date":"2024-10-21","instructor":"Tiffany Blackwell","id":1729383204690,"name":"Vinyasa Flow","locationId":"","capacity":20,"description":"Slow flow vinyasa suitable for all!"}],
 };
