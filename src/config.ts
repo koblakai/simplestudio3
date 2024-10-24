1921bfa7caf9434456eeea367013318394c5ffc2
Simple Studio <noreply@simplestudio.com>
2024-10-24 00:02:06 -0400

    Add/update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,11 @@
 
 export interface PricingPlan {
+  id: string;
   name: string;
   price: number;
   description: string[];
+  durationMonths: number;
+  paymentLink?: string;  // Make paymentLink optional with ?
 }
 
 export interface Config {
@@ -12,7 +15,7 @@ export interface Config {
   enableBlog: boolean;
   enableShop: boolean;
   landingPageImage: string;
-  pricing: PricingPlan[]; // Include pricing in the type definition
+  pricing: PricingPlan[];
 }
 
 export const config: Config = {
@@ -24,54 +27,54 @@ export const config: Config = {
   landingPageImage: "https://images.unsplash.com/photo-1524863479829-916d8e77f114?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
   pricing: [
   {
-    "name": "Monthly Unlimited",
+    "price": 80,
     "description": [
       "Access to all class for one month"
     ],
-    "price": 80
+    "name": "Monthly Unlimited"
   },
   {
+    "name": "Monthly Premium",
     "price": 120,
     "description": [
       "Access to all classes for one month",
       "Yoga mat rental"
-    ],
-    "name": "Monthly Premium"
-  },
-  {
-    "price": 999,
-    "name": "Yearly Unlimited",
-    "description": [
-      "Access to all classes for one year "
     ]
   },
   {
-    "name": "Yearly Premium",
-    "price": 1199,
+    "description": [
+      "Access to all classes for one year "
+    ],
+    "price": 999,
+    "name": "Yearly Unlimited"
+  },
+  {
     "description": [
       "Access to All Classes",
       "Mat Rental",
       "Access to 2 Specialized Workshops"
-    ]
+    ],
+    "price": 1199,
+    "name": "Yearly Premium"
   },
   {
-    "durationMonths": 1,
-    "price": 30,
     "paymentLink": "",
+    "price": 30,
+    "durationMonths": 1,
     "name": "Walk-in ",
     "description": [
       "Access to one class"
     ]
   },
   {
-    "price": 50,
+    "paymentLink": "",
     "description": [
       "Access to one class",
       "Yoga Mat Rental"
     ],
-    "name": "Walk-in premium",
     "durationMonths": 1,
-    "paymentLink": ""
+    "name": "Walk-in premium",
+    "price": 50
   }
-], // Add pricing to the config object
+],
 };
