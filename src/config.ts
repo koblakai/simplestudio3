01ab86a9c720dbd3b3048d477124ef16bb133a25
Simple Studio <noreply@simplestudio.com>
2024-10-23 23:56:55 -0400

    Add/update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -24,10 +24,10 @@ export const config: Config = {
   landingPageImage: "https://images.unsplash.com/photo-1524863479829-916d8e77f114?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
   pricing: [
   {
+    "name": "Monthly Unlimited",
     "description": [
       "Access to all class for one month"
     ],
-    "name": "Monthly Unlimited",
     "price": 80
   },
   {
@@ -39,20 +39,20 @@ export const config: Config = {
     "name": "Monthly Premium"
   },
   {
+    "price": 999,
     "name": "Yearly Unlimited",
     "description": [
       "Access to all classes for one year "
-    ],
-    "price": 999
+    ]
   },
   {
+    "name": "Yearly Premium",
+    "price": 1199,
     "description": [
       "Access to All Classes",
       "Mat Rental",
       "Access to 2 Specialized Workshops"
-    ],
-    "name": "Yearly Premium",
-    "price": 1199
+    ]
   },
   {
     "durationMonths": 1,
@@ -64,14 +64,14 @@ export const config: Config = {
     ]
   },
   {
-    "name": "Walk-in premium",
-    "paymentLink": "",
+    "price": 50,
     "description": [
       "Access to one class",
       "Yoga Mat Rental"
     ],
+    "name": "Walk-in premium",
     "durationMonths": 1,
-    "price": 50
+    "paymentLink": ""
   }
 ], // Add pricing to the config object
 };
