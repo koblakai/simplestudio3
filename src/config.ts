6a69992978f4508dfd135bb81d5e8563846ab543
Simple Studio <noreply@simplestudio.com>
2024-10-24 16:21:05 -0400

    Add/update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   studioName: "Simple Studio",
   locations: [],
-  pricing: [{"name":"Monthly Unlimited","description":["Access to all class for one month"],"price":80},{"price":120,"name":"Monthly Premium","description":["Access to all classes for one month","Yoga mat rental"]},{"price":999,"description":["Access to all classes for one year "],"name":"Yearly Unlimited"},{"name":"Yearly Premium","description":["Access to All Classes","Mat Rental","Access to 2 Specialized Workshops"],"price":1199},{"name":"Walk-in ","paymentLink":"","durationMonths":1,"price":30,"description":["Access to one class"]},{"paymentLink":"","description":["Access to one class","Yoga Mat Rental"],"price":50,"durationMonths":1,"name":"Walk-in premium"}],
+  pricing: [{"description":["Access to all class for one month"],"price":80,"name":"Monthly Unlimited"},{"description":["Access to all classes for one month","Yoga mat rental"],"price":120,"name":"Monthly Premium"},{"price":999,"name":"Yearly Unlimited","description":["Access to all classes for one year "]},{"name":"Yearly Premium","price":1199,"description":["Access to All Classes","Mat Rental","Access to 2 Specialized Workshops"]},{"name":"Walk-in ","price":30,"paymentLink":"","description":["Access to one class"],"durationMonths":1},{"price":50,"paymentLink":"","durationMonths":1,"name":"Walk-in premium","description":["Access to one class","Yoga Mat Rental"]}],
   socialMedia: [],
   enableBlog: false,
   enableShop: false,
