8cc30363037bebb718c70970cbb3a55b42c70d04
Simple Studio <noreply@simplestudio.com>
2024-10-24 10:20:30 -0400

    Add/update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -22,53 +22,53 @@ export interface Config {
 const pricingWithIds = [
   {
     "name": "Monthly Unlimited",
-    "price": 80,
     "description": [
       "Access to all class for one month"
-    ]
+    ],
+    "price": 80
   },
   {
-    "name": "Monthly Premium",
     "price": 120,
+    "name": "Monthly Premium",
     "description": [
       "Access to all classes for one month",
       "Yoga mat rental"
     ]
   },
   {
-    "price": 999,
     "description": [
       "Access to all classes for one year "
     ],
+    "price": 999,
     "name": "Yearly Unlimited"
   },
   {
-    "name": "Yearly Premium",
-    "price": 1199,
     "description": [
       "Access to All Classes",
       "Mat Rental",
       "Access to 2 Specialized Workshops"
-    ]
+    ],
+    "name": "Yearly Premium",
+    "price": 1199
   },
   {
-    "name": "Walk-in ",
     "durationMonths": 1,
+    "price": 30,
     "description": [
       "Access to one class"
     ],
-    "paymentLink": "",
-    "price": 30
+    "name": "Walk-in ",
+    "paymentLink": ""
   },
   {
-    "price": 50,
-    "durationMonths": 1,
     "paymentLink": "",
+    "durationMonths": 1,
+    "name": "Walk-in premium",
+    "price": 50,
     "description": [
       "Access to one class",
       "Yoga Mat Rental"
-    ],
-    "name": "Walk-in premium"
+    ]
   }
 ].map((plan: any, index: number) => ({
   id: plan.id || `plan-${index + 1}`,
