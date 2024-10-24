0c8e51826d6cd7c46325e515ab0d1b35a590ce56
Simple Studio <noreply@simplestudio.com>
2024-10-24 09:09:43 -0400

    Add/update src/firebase/config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,5 @@
 
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -13,7 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { app, auth, db };
